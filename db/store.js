aaf549b626b543cfd675ae6de04edcc7b6048094
Jonathan Arias <JonathanL.Arias@outlook.com>
2020-11-08 01:29:56 -0500

    Store.removeNote finished. Webpage fully functional

--- db/store.js
+++ db/store.js
@@ -28,7 +28,7 @@ class Store {
 
     getNotes() {
         return this.read().then((notes) => {
-            console.log("Console Log: ", notes);
+            // console.log("Console Log: ", notes);
             // Initialized read function that returns info in db.json
             // Then starts new function with the parameter of notes
             let parsedNotes;
@@ -40,15 +40,15 @@ class Store {
             } catch (err) {
                 parsedNotes = [];
             }
-            console.log("Parsed Notes:", parsedNotes);
-            console.log("Inside getNotes");
+            // console.log("Parsed Notes:", parsedNotes);
 
             return parsedNotes;
         });
     }
+    // getNotes works
 
     addNote(note) {
-        console.log("NOTE:", note);
+        // console.log("NOTE:", note);
         // breaking down note object so we could use title and text variable
         const { title, text } = note;
         // If null in title or null in text a new Error is thrown
@@ -58,16 +58,16 @@ class Store {
 
             // Add a unique id to the note using uuid package
             const newNote = { title, text, id: uuidv1() };
-            console.log("NEW NOTE:", newNote);
+            // console.log("NEW NOTE:", newNote);
 
 
             return this.getNotes().then(notes => {
                 notes.push(newNote);
-                console.log("New Notes Array:", notes);
-                console.log("This:", this);
-                this.write("db/db.json",JSON.stringify(notes));
+                // console.log("New Notes Array:", notes);
+                // console.log("This:", this);
+                this.write("db/db.json", JSON.stringify(notes));
                 return newNote;
-            }).catch(err => {console.log("Error:", err)});``
+            }).catch(err => { console.log("Error:", err) });
 
 
             // Get all notes, add the new note, write all the updated notes, return the newNote
@@ -90,14 +90,66 @@ class Store {
 
 
     }
+    // Store.addNotes works!
 
-    // removeNote(id) {
-    //     // Need to define this method
+    removeNote(id) {
+        // Need to define this method
+        console.log("Note ID:", id)
 
-    //     // Read, remove with particular id, rewrite
+        return this.getNotes().then(notes => {
+            console.log("Notes:", notes);
+            const nonRemovedNotes = notes.filter(notes => {
+                if (notes.id !== id) {
+                    // notes.concat(nonRemovedNotes);
+                    // console.log(nonRemovedNotes);
+
+                    // this.write("db/db.json", JSON.stringify(nonRemovedNotes));
+                    return notes;
+                }
+
+                // notes.concat(nonRemovedNotes);
+
+                //     this.write("db/db.json", JSON.stringify(nonRemovedNotes));
+                // return id;
+            })
+
+            console.log("nonremoved:", nonRemovedNotes);
+            this.write("db/db.json", JSON.stringify(nonRemovedNotes));
+            console.log("Final ID:",id);
+            return id;
 
 
-    // }
+
+        }).catch(err => { console.log("Error:", err) });
+
+        // this.write("db/db.json", JSON.stringify(nonRemovedNotes));
+
+        // const canWatchRatedR = moviePatrons.filter(function(patron) {
+        //     return patron.age > 17;
+        //   });
+
+        //   console.log(canWatchRatedR);
+
+
+        // const notesID =
+
+
+        // id has to be passed down
+
+        // id is already given
+
+        // Use filter or map
+
+        // Read, parse, match ids, remove id, rewrite
+
+
+        // Read, remove with particular id, rewrite
+        // Read db.json
+        // FIgure out how pinpoitn a chosen id, and then delete said id
+        // rewrite our new array
+
+
+    }
 
 
 }
